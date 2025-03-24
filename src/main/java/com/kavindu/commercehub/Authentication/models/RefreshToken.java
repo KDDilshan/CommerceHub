a07137bb5ab814e17b1995bdbe53207f1955eb33
KDDilshan <kdprojects847@gmail.com>
2025-03-24 11:15:57 +0530

    added new models for adding security

--- src/main/java/com/kavindu/commercehub/Authentication/models/RefreshToken.java
+++ src/main/java/com/kavindu/commercehub/Authentication/models/RefreshToken.java
@@ -1,4 +1,28 @@
 package com.kavindu.commercehub.Authentication.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class RefreshToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(unique=true, nullable=false)
+    private String refreshToken;
+
+    @Column(nullable=false)
+    private Long expiresIn;
+
+    @OneToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinColumn(name = "user_id",referencedColumnName = "id")
+    @ToString.Exclude
+    private AppUser user;
 }
