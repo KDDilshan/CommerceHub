c83125b538fe65ab922d0a85333e39dbd4bd8a8b
KDDilshan <kdprojects847@gmail.com>
2025-03-24 09:24:17 +0530

    Added logger for deleting product

--- src/main/java/com/kavindu/commercehub/Product/service/DeleteProductService.java
+++ src/main/java/com/kavindu/commercehub/Product/service/DeleteProductService.java
@@ -4,6 +4,8 @@ import com.kavindu.commercehub.Exceptions.ProductNotFoundException;
 import com.kavindu.commercehub.Product.Repositories.ProductRepository;
 import com.kavindu.commercehub.Product.models.Product;
 import com.kavindu.commercehub.Product.service.repos.Querry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +13,9 @@ import java.util.UUID;
 
 @Service
 public class DeleteProductService implements Querry<UUID,String> {
+
+    private final static Logger logger= LoggerFactory.getLogger(DeleteProductService.class);
+
     private final ProductRepository productRepository;
 
     public DeleteProductService(ProductRepository productRepository) {
@@ -19,12 +24,15 @@ public class DeleteProductService implements Querry<UUID,String> {
 
     @Override
     public ResponseEntity<String> execute(UUID uuid) {
+        logger.info("Deleting product with id {}", uuid);
         Product deletingProduct = productRepository.findById(uuid).orElse(null);
         if (deletingProduct!=null) {
             productRepository.delete(deletingProduct);
+            logger.info("Deleted product with id {}", uuid);
             return ResponseEntity.ok("Product deleted successfully");
         }
 
+        logger.info("Product with id {} not found", uuid);
         throw new ProductNotFoundException();
 
     }
