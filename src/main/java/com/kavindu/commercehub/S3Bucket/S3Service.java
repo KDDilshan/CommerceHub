cfda4a9f8b229e6a22b9910cebb16b589a512c84
KDDilshan <kdprojects847@gmail.com>
2025-04-11 10:00:37 +0530

    add: get object function

--- src/main/java/com/kavindu/commercehub/S3Bucket/S3Service.java
+++ src/main/java/com/kavindu/commercehub/S3Bucket/S3Service.java
@@ -1,9 +1,14 @@
 package com.kavindu.commercehub.S3Bucket;
 
 import org.springframework.stereotype.Service;
+import software.amazon.awssdk.core.ResponseInputStream;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectResponse;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+import java.io.IOException;
 
 @Service
 public class S3Service {
@@ -21,6 +26,22 @@ public class S3Service {
                 .key(key)
                 .build();
         s3.putObject(objectRequest, RequestBody.fromBytes(file));
+    }
+
+    public byte[]  getObject(String bucketname,String key){
+        GetObjectRequest getObjectRequest=GetObjectRequest.builder()
+                .bucket(bucketname)
+                .key(key)
+                .build();
+
+        ResponseInputStream<GetObjectResponse> response= s3.getObject(getObjectRequest);
+        try {
+            byte[] bytes= response.readAllBytes();
+            return bytes;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
 
