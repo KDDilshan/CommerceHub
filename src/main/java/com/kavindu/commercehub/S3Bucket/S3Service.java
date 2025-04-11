20dc39e40d7329a66153f3e7d8c8764cc54713e5
KDDilshan <kdprojects847@gmail.com>
2025-04-11 09:59:31 +0530

    add: put object function

--- src/main/java/com/kavindu/commercehub/S3Bucket/S3Service.java
+++ src/main/java/com/kavindu/commercehub/S3Bucket/S3Service.java
@@ -1,5 +1,27 @@
 package com.kavindu.commercehub.S3Bucket;
 
+import org.springframework.stereotype.Service;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+@Service
 public class S3Service {
+
+    private final S3Client s3;
+
+
+    public S3Service(S3Client s3) {
+        this.s3 = s3;
+    }
+
+    public void putObject(String bucketName,String key,byte[] file){
+        PutObjectRequest objectRequest=PutObjectRequest.builder()
+                .bucket(bucketName)
+                .key(key)
+                .build();
+        s3.putObject(objectRequest, RequestBody.fromBytes(file));
+    }
+
 
 }
