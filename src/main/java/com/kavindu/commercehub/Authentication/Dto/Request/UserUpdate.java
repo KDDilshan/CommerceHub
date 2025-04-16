1c1ada1dcbf11160691fd84b762669d202bcf82b
KDDilshan <kdprojects847@gmail.com>
2025-04-16 13:29:27 +0530

    add : new dto for reuest user update

--- src/main/java/com/kavindu/commercehub/Authentication/Dto/Request/UserUpdate.java
+++ src/main/java/com/kavindu/commercehub/Authentication/Dto/Request/UserUpdate.java
@@ -1,2 +1,17 @@
-package com.kavindu.commercehub.Authentication.Dto.Request;public class UserUpdate {
+package com.kavindu.commercehub.Authentication.Dto.Request;
+
+import com.kavindu.commercehub.Authentication.models.AppUser;
+import jdk.jfr.Name;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserUpdate {
+    private Integer id;
+    private AppUser appUser;
 }
