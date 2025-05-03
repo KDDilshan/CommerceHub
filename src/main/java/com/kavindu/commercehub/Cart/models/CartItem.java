29d306caa1208cae79df6cc78d47947784695e08
KDDilshan <kdprojects847@gmail.com>
2025-05-03 23:38:52 +0530

    modifed : add required feilds

--- src/main/java/com/kavindu/commercehub/Cart/models/CartItem.java
+++ src/main/java/com/kavindu/commercehub/Cart/models/CartItem.java
@@ -1,4 +1,29 @@
 package com.kavindu.commercehub.Cart.models;
 
+import com.kavindu.commercehub.Authentication.models.AppUser;
+import com.kavindu.commercehub.Product.models.Product;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class CartItem {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private int quantity;
+
+    @ManyToOne
+    private AppUser user;
+
+    @ManyToOne
+    private Product product;
 }
