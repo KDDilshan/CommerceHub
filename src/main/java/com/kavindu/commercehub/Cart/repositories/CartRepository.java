8aeb700d909bc4e361093764295f3178c74aa7e2
KDDilshan <kdprojects847@gmail.com>
2025-05-06 23:27:37 +0530

    add: the resiotry japa method for cart

---
+++ src/main/java/com/kavindu/commercehub/Cart/repositories/CartRepository.java
@@ -0,0 +1,14 @@
+package com.kavindu.commercehub.Cart.repositories;
+
+import com.kavindu.commercehub.Cart.models.CartItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface CartRepository extends JpaRepository<CartItem, Long> {
+    List<CartItem> findByCustomerId(Long customerId);
+    Optional<CartItem> findByCustomerIdAndProductId(Long customerId, Long productId);
+}
