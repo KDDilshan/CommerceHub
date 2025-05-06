38e1749cdf7ac764ea54db7e7c84175d6ef31fb0
KDDilshan <kdprojects847@gmail.com>
2025-05-06 23:26:19 +0530

    add: the enpoints for cart

---
+++ src/main/java/com/kavindu/commercehub/Cart/Controller/CartController.java
@@ -0,0 +1,40 @@
+package com.kavindu.commercehub.Cart.Controller;
+
+import com.kavindu.commercehub.Cart.Service.CartService;
+import com.kavindu.commercehub.Cart.dtos.CartItemDTO;
+import com.kavindu.commercehub.Cart.models.CartItem;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/carts")
+public class CartController {
+
+    private final CartService cartService;
+
+    public CartController(CartService cartService) {
+        this.cartService = cartService;
+    }
+
+    @GetMapping("/{customerId}")
+    public ResponseEntity<List<CartItem>> getCart(@PathVariable Long customerId) {
+        return cartService.getCartItems(customerId);
+    }
+
+    @PostMapping("/add")
+    public CartItem addToCart(@RequestParam Long customerId, @RequestParam Long productId, @RequestParam int quantity) {
+        return cartService.addToCart(customerId, productId, quantity);
+    }
+
+    @DeleteMapping("/remove/{itemId}")
+    public void removeFromCart(@PathVariable Long itemId) {
+        cartService.removeItem(itemId);
+    }
+
+    @DeleteMapping("/clear/{customerId}")
+    public void clearCart(@PathVariable Long customerId) {
+        cartService.clearCart(customerId);
+    }
+}
