d12634dc85a34574773f23c511a70a9bee9ba85b
KDDilshan <kdprojects847@gmail.com>
2025-05-09 13:30:29 +0530

    update: new get cart item controller

--- src/main/java/com/kavindu/commercehub/Cart/Controller/CartController.java
+++ src/main/java/com/kavindu/commercehub/Cart/Controller/CartController.java
@@ -2,11 +2,13 @@ package com.kavindu.commercehub.Cart.Controller;
 
 import com.kavindu.commercehub.Cart.Service.CartService;
 import com.kavindu.commercehub.Cart.dtos.CartItemDTO;
+import com.kavindu.commercehub.Cart.dtos.DisplayCartDto;
 import com.kavindu.commercehub.Cart.models.CartItem;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/carts")
@@ -19,25 +21,28 @@ public class CartController {
     }
 
     @GetMapping("/{customerId}")
-    public ResponseEntity<List<CartItem>> getCart(@PathVariable Long customerId) {
-        List<CartItem> item= (List<CartItem>) cartService.getCartItems(customerId);
+    public ResponseEntity<List<DisplayCartDto>> getCart(@PathVariable UUID customerId) {
+        List<DisplayCartDto> item = cartService.getCartItems(customerId);
         return ResponseEntity.ok(item);
     }
 
-    @PostMapping("/add")
-    public ResponseEntity<CartItem> addToCart(@RequestParam Long customerId, @RequestParam Long productId, @RequestParam int quantity) {
-        CartItem item = cartService.addToCart(customerId, productId, quantity);
-        return ResponseEntity.ok(item);
-    }
+//    @PostMapping("/add")
+//    public ResponseEntity<CartItem> addToCart(@RequestParam Long customerId, @RequestParam Long productId, @RequestParam int quantity) {
+//        CartItem item = cartService.addToCart(customerId, productId, quantity);
+//        return ResponseEntity.ok(item);
+//    }
 
 
-    @DeleteMapping("/remove/{itemId}")
-    public void removeFromCart(@PathVariable Long itemId) {
-        cartService.removeItem(itemId);
-    }
+//    @DeleteMapping("/remove/{itemId}")
+//    public ResponseEntity<Void> removeFromCart(@PathVariable Long itemId) {
+//        cartService.removeItem(itemId);
+//        return ResponseEntity.noContent().build(); // 204 No Content
+//    }
 
-    @DeleteMapping("/clear/{customerId}")
-    public void clearCart(@PathVariable Long customerId) {
-        cartService.clearCart(customerId);
-    }
+//    @DeleteMapping("/clear/{customerId}")
+//    public ResponseEntity<Void> clearCart(@PathVariable Long customerId) {
+//        cartService.clearCart(customerId);
+//        return ResponseEntity.noContent().build(); // 204 No Content
+//    }
+
 }
