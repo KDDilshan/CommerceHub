39dba6207097175f302756a4a43e0cd6b7b73086
KDDilshan <kdprojects847@gmail.com>
2025-05-09 13:32:08 +0530

    fixed: AppUser id in repository

--- src/main/java/com/kavindu/commercehub/Cart/repositories/CartRepository.java
+++ src/main/java/com/kavindu/commercehub/Cart/repositories/CartRepository.java
@@ -6,9 +6,10 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 @Repository
 public interface CartRepository extends JpaRepository<CartItem, Long> {
-    List<CartItem> findByCustomerId(Long customerId);
+    List<CartItem> findByCustomerId(UUID customerId);
     Optional<CartItem> findByCustomerIdAndProductId(Long customerId, Long productId);
 }
