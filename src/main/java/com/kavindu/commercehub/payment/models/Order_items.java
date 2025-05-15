7dc38ef68aa1fa80b76cccb096d68d66486efb9c
KDDilshan <kdprojects847@gmail.com>
2025-05-15 23:58:54 +0530

    add: data types to entoty

--- src/main/java/com/kavindu/commercehub/payment/models/Order_items.java
+++ src/main/java/com/kavindu/commercehub/payment/models/Order_items.java
@@ -1,4 +1,27 @@
 package com.kavindu.commercehub.payment.models;
 
+import com.kavindu.commercehub.Product.models.Product;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Order_items {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @OneToMany
+    private Orders order;
+
+    @OneToMany
+    private Product product;
+
+    private int quantity;
+
+    private int price;
 }
