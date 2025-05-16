0c081265221538c6a4db96fc9c58b1b175b1bff2
KDDilshan <kdprojects847@gmail.com>
2025-05-16 11:52:27 +0530

    updated: feilds that requred to store shipping details

--- src/main/java/com/kavindu/commercehub/payment/models/Shipping_details.java
+++ src/main/java/com/kavindu/commercehub/payment/models/Shipping_details.java
@@ -1,4 +1,27 @@
 package com.kavindu.commercehub.payment.models;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Shipping_details {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotNull(message = "add Shipping Address")
+    private String shippingAddress;
+
+    @NotNull(message = "enter phone no")
+    private String PhoneNo;
+
+    @OneToOne
+    private Orders order;
+
 }
