e086edec024fb0c07d55e33b8d1bc34bd94ad5fe
KDDilshan <kdprojects847@gmail.com>
2025-05-20 23:25:20 +0530

    add: new order request dto

--- src/main/java/com/kavindu/commercehub/payment/dto/OrderItemRequest.java
+++ src/main/java/com/kavindu/commercehub/payment/dto/OrderItemRequest.java
@@ -1,2 +1,13 @@
-package com.kavindu.commercehub.payment.dto;public class OrderItemRequest {
+package com.kavindu.commercehub.payment.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderItemRequest {
+    private int productId;
+    private int quantity;
 }
