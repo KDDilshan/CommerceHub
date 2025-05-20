58ae511eb207ca13b9ad314724bf9ef275e824db
KDDilshan <kdprojects847@gmail.com>
2025-05-21 00:37:19 +0530

    added order Reposioty id type

--- src/main/java/com/kavindu/commercehub/payment/Repository/OrderRepository.java
+++ src/main/java/com/kavindu/commercehub/payment/Repository/OrderRepository.java
@@ -4,6 +4,8 @@ import com.kavindu.commercehub.payment.models.Orders;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface OrderRepository extends JpaRepository<Orders,Integer> {
+public interface OrderRepository extends JpaRepository<Orders, UUID> {
 }
