0412adc664552785ded5a03cc6737c8cd51fecd4
KDDilshan <kdprojects847@gmail.com>
2025-05-20 23:53:47 +0530

    add: new payment service to add payemnt details to db

---
+++ src/main/java/com/kavindu/commercehub/payment/service/PaymentService.java
@@ -0,0 +1,36 @@
+package com.kavindu.commercehub.payment.service;
+
+import com.kavindu.commercehub.payment.Repository.OrderRepository;
+import com.kavindu.commercehub.payment.Repository.PaymentRepository;
+import com.kavindu.commercehub.payment.dto.PaymentRequest;
+import com.kavindu.commercehub.payment.models.Orders;
+import com.kavindu.commercehub.payment.models.Payment;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class PaymentService {
+
+    private final PaymentRepository paymentRepository;
+    private final OrderRepository orderRepository;
+
+    public PaymentService(PaymentRepository paymentRepository, OrderRepository orderRepository) {
+        this.paymentRepository = paymentRepository;
+        this.orderRepository = orderRepository;
+    }
+
+    public Payment processPayment(PaymentRequest request) {
+        Orders orders=orderRepository.findById(request.getOrderId())
+                .orElseThrow(()->new RuntimeException("Order not found"));
+
+        Payment payment=new Payment();
+        payment.setId(UUID.randomUUID());
+        payment.setOrders(orders);
+        payment.setPaymentMethod(request.getPaymentMethod());
+        payment.setPaymentStatus(request.getPaymentStatus());
+        payment.setTransactionId(request.getTransactionId());
+
+        return paymentRepository.save(payment);
+    }
+}
