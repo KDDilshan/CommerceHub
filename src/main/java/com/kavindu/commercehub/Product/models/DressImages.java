dcfaa18dc8153d2917d98eb8bcae039a7dcdaf49
KDDilshan <kdprojects847@gmail.com>
2025-05-26 23:56:43 +0530

    updated: add new colums to add one or more images to dress

--- src/main/java/com/kavindu/commercehub/Product/models/DressImages.java
+++ src/main/java/com/kavindu/commercehub/Product/models/DressImages.java
@@ -1,4 +1,24 @@
 package com.kavindu.commercehub.Product.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class DressImages {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String imageUrl;
+
+    @ManyToOne
+    private Product product;
 }
